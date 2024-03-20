662e00df6637a4d0bed75dce4e432d023c94bedd
Valentin Uveges <valentin.uveges@gmail.com>
2024-03-21 00:03:11 +0200

    feat(lsp): cleanup duplicate cmp config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -109,69 +109,6 @@ return {
     }
   },
 
-
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        config = function()
-          require("copilot_cmp").setup()
-        end,
-      },
-    },
-    opts = {
-      mapping = {
-        ["<up>"] = require("cmp").mapping.select_prev_item({ behavior = require("cmp").SelectBehavior.Select }),
-        ["<down>"] = require("cmp").mapping.select_next_item({ behavior = require("cmp").SelectBehavior.Select }),
-
-        ["<Tab>"] = require("cmp").mapping(function(fallback)
-          if require("cmp").visible() then
-            local entry = require("cmp").get_selected_entry()
-            if not entry then
-              require("cmp").select_next_item({ behavior = require("cmp").SelectBehavior.Select })
-            end
-            require("cmp").confirm()
-          elseif require("luasnip").expand_or_jumpable() then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = require("cmp").mapping(function(fallback)
-          if require("luasnip").jumpable(-1) then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      },
-
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "copilot" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
-      },
-
-      sorting = {
-        priority_weight = 2,
-        comparators = {
-          require("cmp").config.compare.order,
-          require("cmp").config.compare.offset,
-          require("cmp").config.compare.exact,
-          require("cmp").config.compare.score,
-          require("cmp").config.compare.recently_used,
-          require("cmp").config.compare.locality,
-          require("cmp").config.compare.kind,
-          require("cmp").config.compare.length,
-        },
-      },
-    }
-  },
-
   {
     "neovim/nvim-lspconfig",
     dependencies = {
