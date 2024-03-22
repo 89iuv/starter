bd45cf5478ee64d143be93942f3c639d31e52b35
Valentin Uveges <valentin.uveges@gmail.com>
2024-03-22 08:43:57 +0200

    feat(conform): add shortcut to format a range in visual mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,11 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map(
+  "v",
+  "<leader>fm",
+  '<ESC><cmd>lua require("conform").format { lsp_fallback = true } <CR>',
+  { desc = "Format Files" }
+)
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
